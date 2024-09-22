4718098a529a6521324560bb20042c8680e3115f
jimjasonp <132594623+jimjasonp@users.noreply.github.com>
2024-09-22 11:50:16 +0300

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -4,21 +4,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-for i in range(0,100):
-    sample_sensor =sensor_data_list[i]['s2']
+def sensor_fft(sensor_name):
 
-    fs = 1/750
-    #the sampling frequency is 1/(seconds in a total experiment time)
 
-    fourier = np.fft.fft(sample_sensor)
-    #sample sensor is the value of s2 which is the 
-    freqs = np.fft.fftfreq(sample_sensor.size,d=fs)
+    sensor_fft.sensor_name = []
 
-    power_spectrum = np.abs(fourier)
-    print(max(power_spectrum))
-    #plt.plot(freqs,power_spectrum)
+    for i in range(0,100):
+        sample_sensor =sensor_data_list[i][sensor_name]
 
-    #plt.xlim(0,max(freqs))
-    #plt.title("Power Spectral Density of the Sunspot Number Time Series")
-    #plt.grid(True)
-    #plt.show()
+        fs = 1/750
+        #the sampling frequency is 1/(seconds in a total experiment time)
+
+        fourier = np.fft.fft(sample_sensor)
+        #sample sensor is the value of s2 which is the 
+        freqs = np.fft.fftfreq(sample_sensor.size,d=fs)
+
+        power_spectrum = np.abs(fourier)
+        
+        sensor_fft.sensor_name.append(max(power_spectrum))
+
+        #plt.plot(freqs,power_spectrum)
+
+        #plt.xlim(0,max(freqs))
+        #plt.title("Power Spectral Density of the Sunspot Number Time Series")
+        #plt.grid(True)
+        #plt.show()
+
+
+sensor_fft('s2')
+print(sensor_fft.sensor_name)
+sensor_fft('s3')
+print(sensor_fft.sensor_name)
+sensor_fft('s4')
+print(sensor_fft.sensor_name)
